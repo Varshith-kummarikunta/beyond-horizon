0f074ced74e5642fbe273146327815c808e4526d
Varshith <varshith.kummarikunta@gmail.com>
2026-07-17 20:56:36 +0530

    feat(core): integrate Lenis smooth scrolling

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import App from "./App";
 import "./index.css";
 
-import App from "./App";
+import Lenis from "lenis";
+
+const lenis = new Lenis({
+  duration: 1.2,
+  smoothWheel: true,
+  smoothTouch: false,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
