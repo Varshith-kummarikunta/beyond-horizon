dd048caf3b04211bfe01b3ff6a51c51d1fb4b755
Varshith <varshith.kummarikunta@gmail.com>
2026-07-17 20:32:52 +0530

    Milestone 1 - Loader completed

--- src/components/Loader/LoaderProgress.jsx
+++
@@ -1,7 +0,0 @@
-export default function LoaderProgress({ progress }) {
-  return (
-    <div className="mt-8 text-sm tracking-[0.3em] text-zinc-400">
-      {progress}%
-    </div>
-  );
-}
--- src/components/Loader/LoaderText.jsx
+++
@@ -1,9 +0,0 @@
-export default function LoaderText() {
-  return (
-    <div className="overflow-hidden">
-      <h1 className="text-4xl font-black tracking-[0.4em] uppercase md:text-7xl">
-        Beyond Horizon
-      </h1>
-    </div>
-  );
-}
